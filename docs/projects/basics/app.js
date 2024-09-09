0837cd0fbe5ec1951a3ce3040bb44d5da9d619df
Sylvia Traw <swhittakertraw@college.harvard.edu>
2024-09-09 16:55:17 -0400

    week one code practice

--- docs/projects/basics/app.js
+++ docs/projects/basics/app.js
@@ -1,1 +1,36 @@
-console.log('Hello World')
+let experiment = 'Stroop';
+let welcomeMessage = `Welcome to our ` + experiment +
+    ` experiment! 
+This is a bit silly because I don't 
+really have to do this part but yuh!
+`
+console.log(welcomeMessage)
+
+let trialCount = 20;
+let colors = ['red', 'green', 'blue'];
+
+alert('Welcome to the ' + experiment + ' Experiment')
+
+trialCount = 40
+
+let halfwayCount = trialCount / 2;
+console.log(halfwayCount);
+//Comments should be serving a purpose themselves not just saying what the code is doing
+
+//Another good use of comment is to remind yourself to do something
+
+//TODO: hey so turns out you can also use this
+
+//you can also use comments to leave notes at the end of line
+
+/*
+woop
+woop
+woop
+ */
+console.log(experiment.charAt(0))
+
+let correct = false;
+console.log(typeof correct);
+
+console.log(10 <= 15); //true
