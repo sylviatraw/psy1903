f34ff19f33962cad200fbd1f384d0648fd98c98d
Sylvia Traw <swhittakertraw@college.harvard.edu>
2024-09-10 17:33:51 -0400

    Math response time exercise part 3

--- docs/projects/basics/math-response-time.js
+++ docs/projects/basics/math-response-time.js
@@ -1,7 +1,9 @@
 alert('In this experiment we will measure your response time. You will be shown a series of simple math equations. Answer these equations as quickly and accurately as you can.')
 
 let start = Date.now();
-let question1 = prompt("What is 3 + 4?");
+let randomNum1A = Math.floor(Math.random() * 10) + 1;
+let randomNum1B = Math.floor(Math.random() * 10) + 1;
+let question1 = prompt("What is " + randomNum1A + " + " + randomNum1B + " ?");
 let end = Date.now();
 let responseTime1 = ((end - start) / 1000);
 
@@ -10,14 +12,18 @@ let responseTime1 = ((end - start) / 1000);
 alert("You answered " + question1 + "in " + responseTime1 + " seconds!");
 
 let start2 = Date.now();
-let question2 = prompt("What is 8 + 9?");
+let randomNum2A = Math.floor(Math.random() * 10) + 1;
+let randomNum2B = Math.floor(Math.random() * 10) + 1;
+let question2 = prompt("What is " + randomNum2A + " + " + randomNum2B + " ?");
 let end2 = Date.now();
 let responseTime2 = ((end2 - start2) / 1000);
 
 alert("You answered " + question2 + "in " + responseTime2 + " seconds!");
 
 let start3 = Date.now();
-let question3 = prompt("What is 5 + 6?")
+let randomNum3A = Math.floor(Math.random() * 10) + 1;
+let randomNum3B = Math.floor(Math.random() * 10) + 1;
+let question3 = prompt("What is " + randomNum3A + " + " + randomNum3B + " ?");
 let end3 = Date.now();
 let responseTime3 = ((end3 - start3) / 1000);
 
