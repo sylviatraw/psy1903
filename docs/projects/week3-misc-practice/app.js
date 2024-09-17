93a5813ccc49a085f5259b029416ca524e77d342
Sylvia Traw <swhittakertraw@college.harvard.edu>
2024-09-17 16:48:08 -0400

    Adding an "age" prompt and response for week 3 practice

--- docs/projects/week3-misc-practice/app.js
+++ docs/projects/week3-misc-practice/app.js
@@ -1,6 +1,6 @@
 let num1 = Math.floor(Math.random() * 10) + 1;
 let num2 = Math.floor(Math.random() * 10) + 1;
-let answer = (num1 + num2)
+let answer = (num1 + num2);
 
 
 let response = prompt('What is ' + num1 + " + " + num2 + "?");
@@ -13,6 +13,17 @@ if (response == answer) {
     feedback = 'You were close!';
 } else {
     feedback = 'Incorrect.';
-}
+};
 
 alert(feedback + ' The expected answer is ' + answer + ".");
+
+let age = prompt('How old are you?');
+if (age <= 12) {
+    alert('Child');
+}
+if (age > 12 && age <= 17) {
+    alert('Teenager');
+}
+if (age >= 18) {
+    alert('Adult');
+}
