1a91cce109e6ccf1d8ed231decd45fb401932cf4
Sylvia Traw <swhittakertraw@college.harvard.edu>
2024-09-24 17:42:59 -0400

    Question 4 on week 4 pset

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -1,8 +1,24 @@
 
-function celsiusToFahrenheit(temperature) {
-    let tempInFahrenheit = (temperature * 1.8) + 32;
-    return tempInFahrenheit;
+// function celsiusToFahrenheit(temperature) {
+//     let tempInFahrenheit = (temperature * 1.8) + 32;
+//     return tempInFahrenheit;
+// }
+
+//console.log(celsiusToFahrenheit(10)); // Expected Output : 50
+
+
+
+function convertTemp(temperature, tempType) {
+    let convertedTemp = 0
+    if (tempType != 'c' && tempType != 'f') {
+        alert('Error. Invalid data entered.')
+    }
+    else if (tempType == 'c') {
+        convertedTemp = (temperature * 1.8) + 32;
+    } else {
+        convertedTemp = (temperature - 32) / 1.8
+    }
+    return convertedTemp;
 }
 
-console.log(celsiusToFahrenheit(10)); // Expected Output : 50
-
+console.log(convertTemp(10, 'f')); // Expected Output : 50
