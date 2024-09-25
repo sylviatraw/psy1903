33df1771a4f7294022150a5441a8677289e583d9
Sylvia Traw <swhittakertraw@college.harvard.edu>
2024-09-24 21:26:16 -0400

    Question 8 on week 4 task set

--- docs/projects/week4-task-set/app.js
+++ docs/projects/week4-task-set/app.js
@@ -51,15 +51,37 @@
 //     return randomNumber;
 // }
 
-results = []
+// results = []
 
-function getOddNumbers(numbers) {
-    for (let number of numbers) {
-        if (number % 2 == 1) {
-            results.push(number)
-        }
-    } return results
+// function getOddNumbers(numbers) {
+//     for (let number of numbers) {
+//         if (number % 2 == 1) {
+//             results.push(number)
+//         }
+//     } return results
+// }
+
+// console.log(getOddNumbers([4, 9, 53, 71, 54, 2]));
+
+
+results = [];
+
+function filterNumbers(numbers, numberType) {
+    if (numberType == 'odd') {
+        for (let number of numbers) {
+            if (number % 2 == 1) {
+                results.push(number);
+            }
+        } return results;
+    } else if (numberType == 'even') {
+        for (let number of numbers) {
+            if (number % 2 == 0) {
+                results.push(number);
+            }
+        } return results;
+    } else {
+        alert('Error, response invalid.');
+    }
 }
 
-console.log(getOddNumbers([4, 9, 53, 71, 54, 2]));
-
+console.log(filterNumbers([4, 9, 53, 71, 54, 2], 'evn'));
