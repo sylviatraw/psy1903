27f281ff1775987f2fa841650dc472c2442f72ea
Sylvia Traw <swhittakertraw@college.harvard.edu>
2024-10-08 16:38:13 -0400

    Question 4 on week 6 task set

--- docs/projects/mrt-blocks-jspsych/experiment.js
+++ docs/projects/mrt-blocks-jspsych/experiment.js
@@ -30,6 +30,7 @@ let questionnaire = {
         { prompt: "I enjoy solving math problems.", name: 'MathEnjoyment', labels: likert_scale },
         { prompt: "I find math easy.", name: 'Easy', labels: likert_scale },
     ],
+    data: { collect: true },
 };
 
 timeline.push(questionnaire);
@@ -79,6 +80,57 @@ for (let block of conditions) {
     }
 }
 
+let resultsTrial = {
+    type: jsPsychHtmlKeyboardResponse,
+    choices: ['NO KEYS'],
+    async: false,
+    stimulus: `
+        <h1>Please wait...</h1>
+        <p>We are saving the results of your inputs.</p>
+        `,
+    on_start: function () {
+        //  ⭐ Update the following three values as appropriate ⭐
+        let prefix = 'mrt';
+        let dataPipeExperimentId = 'your-experiment-id-here';
+        let forceOSFSave = false;
+
+        // Filter and retrieve results as CSV data
+        let results = jsPsych.data
+            .get()
+            .filter({ collect: true })
+            .ignore(['stimulus', 'trial_type', 'plugin_version', 'collect'])
+            .csv();
+
+        // Generate a participant ID based on the current timestamp
+        let participantId = new Date().toISOString().replace(/T/, '-').replace(/\..+/, '').replace(/:/g, '-');
+
+        // Dynamically determine if the experiment is currently running locally or on production
+        let isLocalHost = window.location.href.includes('localhost');
+
+        let destination = '/save';
+        if (!isLocalHost || forceOSFSave) {
+            destination = 'https://pipe.jspsych.org/api/data/';
+        }
+
+        // Send the results to our saving end point
+        fetch(destination, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: '*/*',
+            },
+            body: JSON.stringify({
+                experimentID: dataPipeExperimentId,
+                filename: prefix + '-' + participantId + '.csv',
+                data: results,
+            }),
+        }).then(data => {
+            console.log(data);
+            jsPsych.finishTrial();
+        })
+    }
+}
+timeline.push(resultsTrial);
 // Debrief
 let debriefTrial = {
     type: jsPsychHtmlKeyboardResponse,
