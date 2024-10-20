4605d8e6c08e576c4d0fa0706ca909155d407ef6
Sylvia Traw <swhittakertraw@college.harvard.edu>
2024-10-20 16:30:24 -0400

    demo change

--- docs/projects/lexical-decisions/experiment.js
+++ docs/projects/lexical-decisions/experiment.js
@@ -7,7 +7,7 @@ let timeline = [];
 let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse,
     stimulus: `
-    <h1 class= 'instructions'>Welcome to the Lexical Decision Task</h1>
+    <h1 class= 'instructions'>Welcome to the Lexical Decision Task!</h1>
     <p>In this experiment, you will be shown a series of characters and asked to categorize whether the characters make up a word or not.</p>
     <p>There are three parts to this experiment.</p>
     <p class= 'instructions'>Press SPACE to begin the first part.</p>
